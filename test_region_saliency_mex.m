5e567d60d894fe19280673a7c29cf2630a144a74
Boris Schauerte <schauerte@kit.edu>
2013-07-28 19:01:19 -0700

    added the ability to save the generated saliency and difference maps

--- test_region_saliency_mex.m
+++ test_region_saliency_mex.m
@@ -13,6 +13,14 @@
 % @author B. Schauerte
 % @date   2012,2013
 
+%% Options
+do_visualize_differences    = true; % do you want to visualize the differences between the models?
+colormap_arguments          = {'hot'};%{'default'};% % pick your favorite color map
+do_save_saliency_maps       = false; % do you want to save the saliency maps?
+save_saliency_maps_folder   = 'smaps';
+do_save_difference          = false; % do you want to save the difference maps?
+save_difference_maps_folder = 'dmaps';
+
 %% Allow for different center bias combinations
 %    CB_LINEAR  = 0,
 %    CB_PRODUCT = 1
@@ -33,7 +41,7 @@ smethods = { ...
     };
 nmethods = numel(smethods);
 
-%% calculate the saliency maps for all methods
+%% Calculate the saliency maps for all methods
 img_dir = 'examples';
 files   = dir(fullfile(img_dir,'*.jpg'));
 diffs   = zeros(nmethods,nmethods,length(files));
@@ -53,30 +61,95 @@ for i = 1:length(files)
         tic;
         S=region_saliency_mex(IS,smethods{m}{:});
         t=toc;
-        subplot(3,ceil((nmethods+1)/3),1+m); imshow(mat2gray(S)); colormap hot; title([smethods{m}{1} ' (' num2str(t) ')']);
+        subplot(3,ceil((nmethods+1)/3),1+m); imshow(mat2gray(S)); colormap(colormap_arguments{:}); title([smethods{m}{1} ' (' num2str(t) ')']);
         smaps{m}=S;
     end
     drawnow;
 end
 
-%% visualize differences
-do_visualize_differences = true;
-
+%% Visualize differences
 if do_visualize_differences
+    figure('name','saliency map differences');
     for i = 1 %:length(files) % @note: change this if you want to visualize different/more images
-        I_path = fullfile(img_dir,files(i).name);
-        I_orig = I;
-        I      = imresize(I,[400 NaN]);
+        I_path   = fullfile(img_dir,files(i).name);
+        I        = imread(I_path);
+        I_orig   = I;
+        I        = imresize(I,[400 NaN]);
+        IS       = im2single(I);
+        smaps    = cell(1,numel(smethods));
+        for m = 1:nmethods
+            smaps{m} = region_saliency_mex(IS,smethods{m}{:});
+        end
         
         for m = 1:nmethods
+            subplot(nmethods+1,nmethods+1,m+1); imshow(mat2gray(smaps{m})); colormap(colormap_arguments{:}); title([smethods{m}{1} ' (' num2str(t) ')']);
+            subplot(nmethods+1,nmethods+1,(m+1-1)*(nmethods+1) + 1); imshow(mat2gray(smaps{m})); colormap(colormap_arguments{:}); title(smethods{m}{1});
+        end
+        for m = 1:nmethods
             for n = 1:nmethods
-                subplot(nmethods,nmethods,(m-1)*nmethods + n); 
-                if m == n
-                    imshow(I);
-                else
-                    imshow(mat2gray(smaps{m} - smaps{n})); colormap hot; title([smethods{m}{1} ' vs '  smethods{n}{1}]);
-                end
+                    subplot(nmethods+1,nmethods+1,(m+1-1)*(nmethods+1) + (n+1));
+                    if m == n
+                        imshow(I);
+                    else
+                        imshow(mat2gray(smaps{m} - smaps{n})); colormap(colormap_arguments{:}); title([smethods{m}{1} ' vs '  smethods{n}{1}]);
+                    end
             end
         end
     end
+end
+
+%% Save the saliency maps
+if do_save_saliency_maps
+    h = figure;
+    if ~exist(save_saliency_maps_folder,'dir'), mkdir(save_saliency_maps_folder); end
+    for i = 1:length(files)
+        I_path   = fullfile(img_dir,files(i).name);
+        I        = imread(I_path);
+        I_orig   = I;
+        I        = imresize(I,[400 NaN]);
+        IS       = im2single(I);
+        smaps    = cell(1,numel(smethods));
+        for m = 1:nmethods
+            smaps{m} = region_saliency_mex(IS,smethods{m}{:});
+        end
+        
+        for m = 1:nmethods
+            [fp,fn,fe] = fileparts(files(i).name);
+            imshow(dmap); drawnow;
+            %imwrite(mat2gray(smaps{m}),fullfile(save_saliency_maps_folder,sprintf('%s_%s.png',fn,smethods{m}{1})));
+            imwrite(im2uint8(mat2gray(smaps{m})),colormap(colormap_arguments{:}),fullfile(save_saliency_maps_folder,sprintf('%s_%s_%d.png',fn,smethods{m}{1},m)));
+        end
+    end
+    close(h);
+end
+
+%% Save the difference maps
+if do_save_difference
+    h = figure;
+    if ~exist(save_difference_maps_folder,'dir'), mkdir(save_difference_maps_folder); end
+    for i = 1:length(files)
+        I_path   = fullfile(img_dir,files(i).name);
+        I        = imread(I_path);
+        I_orig   = I;
+        I        = imresize(I,[400 NaN]);
+        IS       = im2single(I);
+        smaps    = cell(1,numel(smethods));
+        for m = 1:nmethods
+            smaps{m} = region_saliency_mex(IS,smethods{m}{:});
+        end
+        
+        for m = 1:nmethods
+            for n = 1:nmethods
+                if m == n
+                    continue;
+                end
+                [fp,fn,fe] = fileparts(files(i).name);
+                %dmap = im2uint8(mat2gray(mat2gray(smaps{m}) - mat2gray(smaps{n})));
+                dmap = im2uint8(mat2gray(smaps{m} - smaps{n}));
+                imshow(dmap); drawnow;
+                imwrite(dmap,colormap(colormap_arguments{:}),fullfile(save_difference_maps_folder,sprintf('%s_%s_vs_%s.png',fn,smethods{m}{1},smethods{n}{1})));
+            end
+        end
+    end
+    close(h);
 end
